202106b1fd1bf39e702d6b912e8f06100772670a
pcon0718 <philippe.conway@gmail.com>
2023-06-15 16:39:06 -0700

    first commit

---
+++ redis_user_app.py
@@ -0,0 +1,18 @@
+import redis
+import string
+import random
+
+# initializing size of string
+K = random.randrange(100)
+V = random.randrange(100)
+
+# using random.choices()
+# generating random strings
+key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=K))
+value = ''.join(random.choices(string.ascii_uppercase + string.digits, k=V))
+print(key)
+print(value)
+
+r = redis.Redis(host='redis.conwayhouse.online', port=6379)
+r.set(key,value)
+# r.get('foo')
