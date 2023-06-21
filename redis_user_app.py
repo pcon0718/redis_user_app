75f006ac4d4b5385cc20f41221d5c3e33396c897
pcon0718 <philippe.conway@gmail.com>
2023-06-21 16:49:50 -0700

    Add full path to word list

--- redis_user_app.py
+++ redis_user_app.py
@@ -5,7 +5,8 @@ def get_list_of_words(path):
     with open(path, 'r', encoding='utf-8') as f:
         return f.read().splitlines()
 
-words = get_list_of_words('./wordlist.10000')
+words = get_list_of_words('/home/pconway/python/redis_user_app/wordlist.10000')
+loop_count = random.randrange(30)
 
 ## Select random word for key, value, and the key marked for deletion.
 random_key = random.choice(words)
@@ -14,11 +15,17 @@ random_key_delete = random.choice(words)
 
 ## For debugging purposes only.
 ## Print the key, value, and the key marked for deletion.
-# print(random_key)
-# print(random_value)
-# print(random_key_delete)
+# print(loop_count)
+print('Key to be created: ', random_key)
+print('Random Value: ', random_value)
+print('Key to be Deleted: ', random_key_delete)
+
+# i = 0
+# while i < loop_count
+#     print("hello")
+#     i += 1
 
 ## Open up a connection to redis. Write a new key/value pair, and delete a random key.
 r = redis.Redis(host='redis', port=6379)
 r.set(random_key,random_value)
-r.delete(random_key)
+r.delete(random_key_delete)
